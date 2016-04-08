f9020e15b7b2cdc2ade18873a44bc3982ec7702b
Amma <Amma@DESKTOP-0B6K0OI>
2016-04-08 17:27:44 +0530

    cokmmm

---
+++ tttt/src/tttt/ggggggggggggg.java
@@ -0,0 +1,12 @@
+package tttt;
+
+public class ggggggggggggg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("ggggggggggggggg");
+
+	}
+
+}
