8a75b56d33282545e7faabcd327c11f08497e5a3
Amma <Amma@DESKTOP-0B6K0OI>
2016-04-09 08:08:40 +0530

    initial changes.

---
+++ SampleProject/src/module.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class module {
+	 public static void main (String[] args) {
+			System.out.println ("Hello, it's: ");
+			System.out.println(new Date());
+		    }
+		
+
+}
